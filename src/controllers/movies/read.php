5799468b6a23ef22450cfa40cccc69ac87828558
Adrian Edy <adrianedypratama@gmail.com>
2022-01-08 00:49:27 +0800

    Add movies query filter

--- src/controllers/movies/read.php
+++ src/controllers/movies/read.php
@@ -2,8 +2,25 @@
 
 $limit = (int) url_query('limit') ?? 10;
 
+$filter = [];
+$rated = url_query('rated');
+$countries = url_query('countries');
+$languages = url_query('languages');
+
+if ($rated) {
+    $filter['rated'] = $rated;
+}
+
+if ($countries) {
+    $filter['countries'] = $countries;
+}
+
+if ($languages) {
+    $filter['languages'] = $languages;
+}
+
 $cursor = db_collection('movies')->find(
-    [],
+    $filter,
     [
         'limit' => $limit,
     ]
